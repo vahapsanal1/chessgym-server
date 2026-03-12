3a64ecf57a894729bb711852d42a090cfd23e979
vahapsanal1 <vahapsanal@gmail.com>
2026-03-13 02:41:43 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 
 # Update these values when releasing a new version
-VERSION = "1.7"
+VERSION = "2.1"
 DOWNLOAD_URL = ""  # Leave empty to serve from this server, or set an external URL
 
 UPDATES_DIR = os.path.dirname(__file__)
